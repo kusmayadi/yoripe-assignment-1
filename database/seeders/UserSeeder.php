639da68f01500d7d82748411dbe14066e490244b
Kusmayadi <kusmayadi@gmail.com>
2023-03-24 21:12:42 +0700

    Add Role & User Seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // admin
+        $admin = User::factory()->create([
+            'email' => 'admin@yoripe.com',
+            'password' => Hash::make('!OrdinaryAdmin')
+        ]);
+
+        $admin->assignRole('admin');
+
+        if (config('app.env') != 'production') {
+            // manager
+            $manager = User::factory()->create([
+                'email' => 'manager@yoripe.com',
+                'password' => Hash::make('!OrdinaryManager')
+            ]);
+
+            $manager->assignRole('manager');
+
+            // user
+            $user = User::factory()->create([
+                'email' => 'user@yoripe.com',
+                'password' => Hash::make('JustOrdinaryUser#1')
+            ]);
+        }
+    }
+}
