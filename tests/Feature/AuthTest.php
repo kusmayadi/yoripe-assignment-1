1cdce6c2f9bfaf31c48e0fc0083a41dfa1eb9f10
Kusmayadi <kusmayadi@gmail.com>
2023-03-24 22:51:25 +0700

    Add login & logout

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_login(): void
+    {
+        $password = 'simple';
+
+        $user = User::factory()->create([
+            'password' => Hash::make($password)
+        ]);
+
+        $response = $this->post('/api/login', [
+            'email' => $user->email,
+            'password' => $password
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertJsonCount(3);
+        $response->assertJsonStructure(['message', 'access_token', 'token_type']);
+    }
+
+    public function test_logout(): void
+    {
+        $password = 'simple';
+
+        $user = User::factory()->create([
+            'password' => Hash::make($password)
+        ]);
+
+        $responseLogin = $this->post('/api/login', [
+            'email' => $user->email,
+            'password' => $password
+        ]);
+
+        $responseLogin->assertStatus(200);
+
+        $token = $responseLogin->json('access_token');
+
+        $responseLogout = $this->withToken($token)->post('/api/logout');
+
+        $responseLogout->assertStatus(200);
+
+        $this->assertEquals(0, $user->tokens()->count());
+    }
+}
